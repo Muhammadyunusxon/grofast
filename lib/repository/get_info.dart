6b31d8914edcdca92e08756c42922222d3ee9249
Muhammadyunusxon <Muhammadyunusxonabdumannopov9@gmail.com>
2023-01-10 08:44:09 +0500

    add repository

---
+++ lib/repository/get_info.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../Model/ProductModel.dart';
+
+abstract class GetInfo {
+  GetInfo._();
+
+  static Future<List<ProductModel?>?> getProduct() async {
+    try {
+      final url = Uri.parse("https://fakestoreapi.com/products");
+      final res = await http.get(url);
+      return productModelFromJson(res.body);
+    } catch (e) {
+      print(e);
+    }
+    return null;
+  }
+
+  static Future<List<String?>?> getCategories() async {
+    try {
+      final url = Uri.parse("https://fakestoreapi.com/products/categories");
+      final res = await http.get(url);
+      return json.decode(res.body) == null
+          ? []
+          : List<String?>.from(json.decode(res.body)!.map((x) => x));
+    } catch (e) {
+      print(e);
+    }
+    return null;
+  }
+
+  static Future<List<ProductModel?>?> getCategoriesProduct(String category) async {
+    try {
+      final url = Uri.parse("https://fakestoreapi.com/products/category/${category}");
+      final res = await http.get(url);
+      return productModelFromJson(res.body);
+    } catch (e) {
+      print(e);
+    }
+    return null;
+  }
+
+
+
+}
