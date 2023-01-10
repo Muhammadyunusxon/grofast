dcee1bd0069715e53c426c788a97ece9bf04d2ba
Muhammadyunusxon <Muhammadyunusxonabdumannopov9@gmail.com>
2023-01-10 08:35:09 +0500

    add Splash Screen

---
+++ lib/Pages/ProductTour/WelcomePage.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:grofast/Pages/ProductTour/ProducTourPage.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).push(
+        MaterialPageRoute(builder: (_) => const TourPage()),
+      );
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (_) => const TourPage()),
+        );
+      },
+      child: Image.asset(
+        'images/tour/Welcome Screen.png',
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
