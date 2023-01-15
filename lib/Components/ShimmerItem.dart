1ae9568952bf91b30778c3c8a6562b8fe0f48b18
Muhammadyunusxon <Muhammadyunusxonabdumannopov9@gmail.com>
2023-01-15 19:12:14 +0500

    add Shimmer

---
+++ lib/Components/ShimmerItem.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+import '../Style/style.dart';
+
+class ShimmerItem extends StatelessWidget {
+  final double height;
+  final double width;
+  const ShimmerItem({Key? key, required this.height, required this.width}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: Shimmer.fromColors(
+        baseColor: Style.shimmerBaseColor,
+        highlightColor:  Style.shimmerHighlightColor,
+        child: Container(
+            width: width,
+            height: height,
+            color: Style.shimmerColor
+
+        ),
+      ),
+    );
+  }
+}
